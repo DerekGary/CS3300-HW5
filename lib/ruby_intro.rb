ac404b1ff629908c5782b83d14ca5f84940ada2a
Derek Gary <jgary@uccs.edu>
2022-10-26 04:22:27 +0000

    Create methods for part 2 & part 3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -46,19 +46,67 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return("Hello, " << name)
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  is_match = s.capitalize.match(/\A+[AEIOU]/)
+  is_letter = s.match(/\A+[A-Za-z]/)
+  if(s == '')
+    return false
+  end
+  if(!is_letter)
+    return false
+  end
+  if(is_match)
+    return false
+  else
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if(s !~ /[^01]/ && s != '')
+    s = s.to_i(2)
+
+    if(s%4 == 0)
+      return true
+    else
+      return false
+    end
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+attr_reader :isbn, :price
+
+def price=(price)
+  if ((price.is_a? Numeric) && (price > 0))
+    price = price.to_f
+  else
+    raise ArgumentError.new("Price needs to be a number greater than zero.")
+  end
+  @price = price
+end
+
+def isbn=(isbn)
+  if isbn == ''
+    raise ArgumentError.new("No ISBN number found")
+  end
+  @isbn = isbn
+end
+
+def initialize(isbn, price)
+  self.isbn = isbn
+  self.price = price
+end
+
+def price_as_string
+  return "$" + sprintf("%.2f", self.price)
+end
+
 end
